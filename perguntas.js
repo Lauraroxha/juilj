7bf2e413e0e8a6d979d1b82704bfa966d6decd62
Lauraroxha <lauraroxha1@gmail.com>
2024-12-01 20:54:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Estilos',
+    'Qual foi uma das bandas mais famosas de 2010?',
+    'Foi a The Black Keys'
+)
+
+criaCartao(
+    'Números',
+    'Quantas músicas o Bruno Mars tem?',
+    'Ele tem um total de 22 músicas em sua carreira'
+)
+
+criaCartao(
+    'Idades',
+    ' Com quanto anos, Michael Jackson (Rei do Pop) Morreu?',
+    '50 anos quando o músico faleceu'
+)
+
+
+criaCartao(
+    'Fama',
+    'Qual a música top 1 no Brasil?',
+    'A noite de Gusttavo Lima'
+)
